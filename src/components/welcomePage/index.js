fcaa1c8d74acbd8bb4d6c01e33885f8082261480
Sarah Moradi <moradisarah2001@gamil.com>
2022-12-17 16:30:41 +0330

    completed welcome page design

--- src/components/welcomePage/index.js
+++ src/components/welcomePage/index.js
@@ -1,5 +1,29 @@
+import './index.css'
+
+import {HiOutlineArrowRight} from 'react-icons/hi'
+
 const WelcomePage = () => {
-  return <>WelcomePage</>
+  return (
+    <>
+    <div className='top-circle'></div>
+      <div className='welcome-content-container'>
+      <div className='d-flex justify-content-center welcome-image-container'>
+        <img src='/images/Checklist.jpg' alt='welcome-icon' width={300} />
+      </div>
+      <div className="welcome-page-title">
+        Get things done.
+      </div>
+      <div className="welcome-page-sub-title">
+        Just a click away from planning your tasks.
+      </div>
+      </div>
+      <div className='bottom-circle'>
+        <div className='h-100 d-flex justify-content-center align-items-center next-icon-container'>
+        <HiOutlineArrowRight size={40} color="#fff" />
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default WelcomePage
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App'
 import {BrowserRouter} from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
