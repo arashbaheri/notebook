0582801ac2cff68e52d9803f9876eb4cc0ab4cbf
Sarah Moradi <moradisarah2001@gamil.com>
2022-12-21 00:23:47 +0330

    added calender to home page design

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,12 +1,33 @@
 import './index.css'
+import 'react-modern-calendar-datepicker/lib/DatePicker.css'
 
+import React, { useState } from 'react'
+
+import { Calendar } from 'react-modern-calendar-datepicker'
 import Header from '../../layout/header'
 import Navbar from '../../layout/navbar'
 
 const HomePage = () => {
+  const [selectedDayRange, setSelectedDayRange] = useState({
+    from: {day: 6, month: 12, year: 2022},
+    to: {day: 14, month: 12, year: 2022},
+  })
+  {
+    console.log(selectedDayRange)
+  }
   return (
     <Navbar>
       <Header>
+        <div className='mt-4 mx-2 calender-container'>
+          <Calendar
+            className='calender-style'
+            value={selectedDayRange}
+            onChange={setSelectedDayRange}
+            shouldHighlightWeekends
+            colorPrimary="#7967b1"
+            colorPrimaryLight="#e3def0"
+          />
+        </div>
       </Header>
     </Navbar>
   )
